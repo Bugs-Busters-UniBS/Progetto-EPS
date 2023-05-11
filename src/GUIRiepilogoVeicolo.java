f65c06fd86e54474233978e89a61e4d4e06b3837
Flavio Giordani <f.giordani003@studenti.unibs.it>
2023-05-11 15:52:11 +0200

    Implementato prototipo di targa

--- src/GUIRiepilogoVeicolo.java
+++ src/GUIRiepilogoVeicolo.java
@@ -3,23 +3,25 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 
 import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+
 public class GUIRiepilogoVeicolo extends JFrame {
 
     //TODO fixare targa
 
-    private JPanel pannelloDati = new JPanel(new GridLayout(8,1));
+    private JPanel pannelloDati = new JPanel(new GridLayout(7,1));
     private JPanel pannelloImmagine = new JPanel(new BorderLayout());
 
     public GUIRiepilogoVeicolo(String titolo, Veicolo veicolo) {
         super(titolo);
         this.setLayout(new BorderLayout(10,0));
-        this.setSize(500,300);
+        this.setSize(600,300);
         this.setResizable(false);
 
         //creazione label
@@ -28,7 +30,7 @@ public class GUIRiepilogoVeicolo extends JFrame {
         JLabel marca = new JLabel("Marca: " + veicolo.getMarca());
         JLabel modello = new JLabel("Modello: " + veicolo.getModello());
         JLabel paese = new JLabel("Paese di Provenienza: " + veicolo.getTarga().getPaese().name());
-        JLabel targa = new JLabel("Targa: " + veicolo.getTarga().getNumero());
+        //JLabel targa = new JLabel("Targa: " + veicolo.getTarga().getNumero());
         JLabel porte;
         JLabel portata;
         JLabel cilindrata;
@@ -38,12 +40,15 @@ public class GUIRiepilogoVeicolo extends JFrame {
 
         //aggiunta delle label costanti al panel
         titoloGUI.setFont(new Font("Lucida Grande", Font.ITALIC, 20));
-        pannelloDati.add(titoloGUI, BorderLayout.PAGE_START);
+        pannelloDati.add(titoloGUI);
+        tipo.setFont(new Font("Serif", Font.PLAIN, 16));
         pannelloDati.add(tipo);
+        marca.setFont(new Font("Serif", Font.PLAIN, 16));
         pannelloDati.add(marca);
+        modello.setFont(new Font("Serif", Font.PLAIN, 16));
         pannelloDati.add(modello);
-        pannelloDati.add(paese);
-        pannelloDati.add(targa);
+        //pannelloDati.add(paese);
+        
 
         //riconoscimento veicolo e istanziazione dei label mancanti
         if(veicolo instanceof Automobile){
@@ -62,13 +67,16 @@ public class GUIRiepilogoVeicolo extends JFrame {
             cilindrata = new JLabel("Cilindrata: " + String.valueOf(moto.getCilindrata()));
             pannelloDati.add(cilindrata);
         }
+        TargaPanel targa = new TargaPanel(veicolo.getTarga());
+        pannelloDati.add(targa);
 
         //aggiunta del bottone di chiusura e della sua azione
-        pannelloDati.add(closeButton);
+        pannelloDati.add(closeButton, BorderLayout.PAGE_END);
         closeButton.addActionListener(ev -> this.dispose());
 
         //aggiunta del panel al frame
         add(pannelloDati, BorderLayout.CENTER);
+        
 
         //aggiunta dell'immagine al panel
         try {
@@ -84,5 +92,29 @@ public class GUIRiepilogoVeicolo extends JFrame {
         //aggiunta del panel alla gui
         add(pannelloImmagine, BorderLayout.LINE_START);
 
+    }
+
+    private class TargaPanel extends JPanel {
+
+        TargaPanel(Targa t) {
+            this.setLayout(new FlowLayout(FlowLayout.LEFT));
+            JPanel pannelloTarga = new JPanel();
+            add(pannelloTarga);
+            pannelloTarga.setBorder(BorderFactory.createLineBorder(Color.black));
+            pannelloTarga.setPreferredSize(new Dimension(150, 35));
+            pannelloTarga.setLayout(new BorderLayout());
+            JPanel leftPanel = new JPanel();
+            leftPanel.setBackground(new Color(0,61,163,255));
+            JPanel rightPanel = new JPanel();
+            rightPanel.setBackground(new Color(0,61,163,255));
+            JLabel targaLabel = new JLabel(t.getNumero());
+            JPanel centerPanel = new JPanel();
+            centerPanel.setBackground(new Color(255,255,255,255));
+            centerPanel.add(targaLabel);
+            pannelloTarga.add(centerPanel,BorderLayout.CENTER);
+    
+            pannelloTarga.add(leftPanel,BorderLayout.LINE_START);
+            pannelloTarga.add(rightPanel,BorderLayout.LINE_END);
+        }
     }
 }
